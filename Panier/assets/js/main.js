0b548df7dfc672523545d4ff9dc17489c29edb1e
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2025-12-04 12:30:37 +0100

    updates cart

--- Panier/assets/js/main.js
+++ Panier/assets/js/main.js
@@ -1,8 +1,11 @@
 const body = document.body,
 header = body.querySelector('header'),
 cart = header.querySelector('.cart'),
+cartItemsDisplay = cart.querySelector('.nbItem'),
 cartContainer = body.querySelector('section.cart'),
-close_cart_content = cartContainer.querySelector('.close_cart_content')
+close_cart_content = cartContainer.querySelector('.close_cart_content'),
+cartContent = cartContainer.querySelector('.cartContent'),
+cartProducts = []
 
 
 
@@ -28,20 +31,82 @@ async function fetchProducts(){
     return products
 }
 
-
+/**
+ * Affiche les chaises dans la page web
+ */
 function setProducts(){
     const articleTemplate = document.querySelector('template#articleTemplate'),
     productsContainer = document.querySelector('.productsContainer')
 
     fetchProducts().then( products =>{
         products.forEach( product => {
-            let template = articleTemplate.content.cloneNode(true)
+            let template = articleTemplate.content.cloneNode(true),
+            btnAdd = template.querySelector('.addToCart')
             template.querySelector('img').src = 'assets/'+product.image
             template.querySelector('h1').innerText = product.name
-            template.querySelector('.price').innerText = product.price+'€'       
+            template.querySelector('.price').innerText = product.price+'€' 
+            btnAdd.dataset.id = product.id
+            btnAdd.dataset.price = product.price
+            btnAdd.dataset.name = product.name
+            btnAdd.dataset.img = 'assets/'+product.image
+            btnAdd.addEventListener('click', function(e){
+                addToCart(e)
+            })
             productsContainer.append(template)
         })
     })
+}
+
+
+/**
+ * Ajoute un article au panier "machine"
+ * @param {clickEvent} e 
+ */
+function addToCart(e){
+    e.preventDefault()
+    const item = e.target.dataset
+    const isInCart = cartProducts.find( p=> p.id === item.id )
+    if(isInCart) isInCart.qty += 1
+        else cartProducts.unshift( {...item,qty:1} )
+    cartUI()
+}
+
+function deleteFromCart(){
+    
+}
+
+function manageQty(){
+    
+}
+
+/**
+ * Affiche visuellement le panier à l'utilisateur en se basant sur le panier "machine"
+ */
+function cartUI(){
+    displayCartContent()
+    displayItemsInCart()
+}
+
+function displayItemsInCart(){
+    cartItemsDisplay.innerText = cartProducts.length
+}
+
+function displayCartContent(){
+    const itemTemplate = document.querySelector('template#item-cart'),
+    fragment = document.createDocumentFragment()
+
+    cartContent.innerHTML = ''
+    cartProducts.forEach(product =>{
+        let template = itemTemplate.content.cloneNode(true)
+        template.querySelector('.item_img img').src = product.img
+        template.querySelector('.item_title').innerText = product.name
+        template.querySelector('.item_price').innerText = product.price+'€'
+        template.querySelector('.item_qty_value').innerText = product.qty
+
+        fragment.append(template)
+    })
+
+    cartContent.append(fragment)
 }
 
 
--- Panier/assets/js/products.js
+++ Panier/assets/js/products.js
@@ -1,7 +1,7 @@
 [
     {
         "id": 1,
-        "name":" LD01 LOUNGE CHAIR",
+        "name":"LD01 LOUNGE CHAIR",
         "price": 200,
         "image": "images/1.png",
         "description": "Expertly rendered by Carl Hansen & Søn, the lounge chair—first introduced in 1951 and enduring ever since—is available in oak or as a combination of oak and walnut, sourced from sustainable forestry. Choose from seat and back upholstery in a selection of leather options or in a custom fabric.",
