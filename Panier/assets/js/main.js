46b69fdcb65c9aed0f456d922ea1d04a9cac76d8
Nicolas Fréret <contact.nicolasfreret@gmail.com>
2025-12-04 16:43:58 +0100

    updates cart

--- Panier/assets/js/main.js
+++ Panier/assets/js/main.js
@@ -4,8 +4,9 @@ cart = header.querySelector('.cart'),
 cartItemsDisplay = cart.querySelector('.nbItem'),
 cartContainer = body.querySelector('section.cart'),
 close_cart_content = cartContainer.querySelector('.close_cart_content'),
+cart_total = cartContainer.querySelector('.cart_total'),
 cartContent = cartContainer.querySelector('.cartContent'),
-cartProducts = []
+cartProducts = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
 
 
 
@@ -67,16 +68,31 @@ function addToCart(e){
     const item = e.target.dataset
     const isInCart = cartProducts.find( p=> p.id === item.id )
     if(isInCart) isInCart.qty += 1
-        else cartProducts.unshift( {...item,qty:1} )
+        else cartProducts.unshift( {...item,qty:1,price: Number(item.price)} )
     cartUI()
 }
 
-function deleteFromCart(){
-    
+/**
+ * Suprrime l'article du panier après une confirmation
+ * 
+ * @param {clickEvent} e 
+ */
+function deleteFromCart(e){
+    const index = Number(e.currentTarget.parentElement.dataset.index)
+    confirm("Supprimer l'article ?") ? cartProducts.splice(index,1) : ''
+    cartUI()
 }
 
-function manageQty(){
-    
+function manageQty(e){
+    e.preventDefault()
+    const btn = e.currentTarget,
+    index = Number(btn.parentElement.parentElement.dataset.index),
+    isAdd = btn.classList.contains('add')
+
+    if(isAdd) cartProducts[index].qty += 1
+        else if( cartProducts[index].qty > 1 ) cartProducts[index].qty -= 1
+            else deleteFromCart(e)
+    cartUI()
 }
 
 /**
@@ -85,20 +101,52 @@ function manageQty(){
 function cartUI(){
     displayCartContent()
     displayItemsInCart()
+    displayCartTotal()
+    saveCart()
 }
 
+/**
+ * Sauvegarder le panier dans la navigateur
+ */
+function saveCart(){
+    localStorage.setItem('cart', JSON.stringify(cartProducts))
+}
+
+/**
+ * Remet à jour le nombre d'article dans le panier dans l'icone du header
+ */
 function displayItemsInCart(){
-    cartItemsDisplay.innerText = cartProducts.length
+    cartItemsDisplay.innerText = cartProducts.reduce( (acc, item) => item.qty + acc, 0 )
 }
 
+/**
+ * Calcul et affiche le montant du total du panier
+ */
+function displayCartTotal(){
+    cart_total.innerText = cartProducts.reduce(function(acc, item){
+        return (item.qty * item.price) + acc
+    },0)
+}
+
+
+/**
+ * Remet à jour le panier visible sur la page
+ */
 function displayCartContent(){
     const itemTemplate = document.querySelector('template#item-cart'),
     fragment = document.createDocumentFragment()
 
     cartContent.innerHTML = ''
-    cartProducts.forEach(product =>{
-        let template = itemTemplate.content.cloneNode(true)
+    cartProducts.forEach((product,i) =>{
+        let template = itemTemplate.content.cloneNode(true),
+        trshBtn = template.querySelector('.item_trash'),
+        qtyBtns = template.querySelectorAll('.item_qty_button')
+    
         template.querySelector('.item_img img').src = product.img
+        trshBtn.parentElement.dataset.index = i
+        qtyBtns[0].addEventListener('click', e=> manageQty(e))
+        qtyBtns[1].addEventListener('click', e=> manageQty(e))
+        trshBtn.addEventListener('click',e => deleteFromCart(e))
         template.querySelector('.item_title').innerText = product.name
         template.querySelector('.item_price').innerText = product.price+'€'
         template.querySelector('.item_qty_value').innerText = product.qty
@@ -111,6 +159,7 @@ function displayCartContent(){
 
 
 setProducts()
+cartUI()
 
 
 
